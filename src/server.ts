7d329f89e5b1e64abbe7f9b1ace4d6dcaf08e0ed
fabriciops <fabriciopps19@gmail.com>
2022-05-29 21:19:31 -0300

    Mustache

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express, {Request, Response} from 'express';
+
+import path from 'path';
+import mustache from 'mustache-express';
 import mainRoutes from './routes/index';
 import mainPainel from './routes/painel';
 
 const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
 
 server.use('/painel',mainPainel);
 
